f9c0f8edfd617c1ca708453740ce65e0d8daecf6
gusleolizarraga1 <126335084+gusleolizarraga1@users.noreply.github.com>
2023-02-25 10:56:15 -0300

    Add files via upload

---
+++ letras.py
@@ -0,0 +1,20 @@
+lista = ['manzana', 'pera', 'banana','durazno','kiwi']
+
+for fruta in lista:
+    if fruta == 'manzana':
+        print("Hay manzanas en la lista ")
+
+
+#for i in range (1,1144):
+#    print(i)
+
+
+
+
+#Simbolos lógicos
+#igual : ==
+#distinto: !=
+#mayor : >
+#menor : <
+#mayor o igual: >=
+#menor o igual : <=
